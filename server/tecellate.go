5a00157c325c4d40714a3df97c110daf4e694c57
Stephen Johnson <srj15@case.edu>
2010-10-25 12:39:33 -0400

    Starting to set up TCP connections

--- server/tecellate.go
+++ server/tecellate.go
@@ -7,25 +7,34 @@ import (
 	"io/ioutil"
 	"log"
 	"strconv"
+	"net"
 )
 
 type Config struct {
 	CoordPath string
+	NumCoords uint
+	ListenPort uint
 }
-
-var config = Config{""}
 
 func main() {
-	loadConfig()
-	listenerReady := make(chan int)
-	go listenForCoordinators(listenerReady)
+	config := loadConfig()
+	listenerReady := make(chan bool, 1)
+	connections := map[int]*net.TCPConn{}
+	
+	go listenForCoordinators(config, listenerReady, connections)
+	
 	fmt.Println("Launched getCoordinators, waiting for ok...")
+	
 	_ = <- listenerReady
+	
 	fmt.Println("Sweet, let's go")
-	launchCoordinators()
+	
+	launchCoordinators(config)
 }
 
-func loadConfig() {
+func loadConfig() Config {
+	var config = Config{"", 0, 4009}
+	
 	configFile, err := os.Open("config.json", os.O_RDONLY, 0)
 	if err != nil {
 		log.Exit(err)
@@ -38,13 +47,15 @@ func loadConfig() {
 	} else {
 		json.Unmarshal(configBytes, &config)
 	}
+	return config
 }
 
-func launchCoordinators() {
+func launchCoordinators(config Config) {
 	wd, err := os.Getwd()
 	for i := 0; i < 10; i++ {
-		_, err = os.ForkExec(config.CoordPath, []string{strconv.Itoa(i)}, nil, 
-							 wd, []*os.File{nil, os.Stdout, os.Stderr})
+		argv := []string{strconv.Itoa(i), strconv.Uitoa(config.ListenPort)}
+		_, err = os.ForkExec(config.CoordPath, argv, 
+							 nil, wd, []*os.File{nil, os.Stdout, os.Stderr})
 		if err != nil {
 			log.Printf("Error on process %d:", i)
 			log.Exit(err)
@@ -52,7 +63,22 @@ func launchCoordinators() {
 	}
 }
 
-func listenForCoordinators(ready chan int) {
+func listenForCoordinators(config Config, ready chan bool, connections map[int]*net.TCPConn) {
+	addr, err := net.ResolveTCPAddr("127.0.0.1:4009");
+	if err != nil { log.Exit(err) }
+	listener, err := net.ListenTCP("tcp", addr);
+	if err != nil { log.Exit(err) }
+	
+	for {
+		conn, err := listener.AcceptTCP();
+		if err != nil {
+			log.Println("error in Accept():", err)
+		} else {
+			conn.SetKeepAlive(true);
+			conn.SetReadTimeout(30000);
+		}
+	}
+	
 	fmt.Println("OK TO ISSUE")
-	ready <- 1
+	ready <- true
 }
