b3e82483e379f0d08cc11221332b1b600cfb2dfd
Stephen Johnson <srj15@case.edu>
2010-10-25 00:30:24 -0400

    Reading json config file

--- server/tecellate.go
+++ server/tecellate.go
@@ -1,8 +1,33 @@
 package main
 
-import fmt "fmt"  // Package implementing formatted I/O.
+import (
+    "fmt"
+    "os"
+    "json"
+    "io/ioutil"
+    "log"
+)
 
+type Config struct {
+    CoordPath string
+}
 
 func main() {
-	fmt.Printf("Hello, world; or Καλημέρα κόσμε; or こんにちは 世界\n")
+    fmt.Printf("hello\n")
+	var config = Config{""}
+	
+	configFile, err := os.Open("config.json", os.O_RDONLY, 0)
+	if err != nil {
+        return
+    }
+    defer configFile.Close()
+    
+    configBytes, err := ioutil.ReadAll(configFile)
+	if err != nil {
+        log.Exit(err)
+    } else {
+        json.Unmarshal(configBytes, &config)
+    }
+    fmt.Printf("%s\n", config.CoordPath)
+    fmt.Printf("goodbye\n")
 }
