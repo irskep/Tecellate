05f4935262400575745b43461a831532c489ebe1
Stephen Johnson <srj15@case.edu>
2010-10-25 13:33:02 -0400

    TCP connections are all set up

--- coord/coord.go
+++ coord/coord.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"net"
+	"log"
 )
 
 func main() {
 	fmt.Printf("Coordinator %s\n", os.Args[0])
+	addr, err := net.ResolveTCPAddr(os.Args[1]);
+	if err != nil { log.Printf("%s: ", os.Args[0]); log.Exit(err) }
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil { log.Printf("%s: ", os.Args[0]); log.Exit(err) }
+	conn.Write([]uint8(os.Args[0]))
+	conn.Close()
 }
--- server/tecellate.go
+++ server/tecellate.go
@@ -8,18 +8,19 @@ import (
 	"log"
 	"strconv"
 	"net"
+	"time"
 )
 
 type Config struct {
 	CoordPath string
-	NumCoords uint
-	ListenPort uint
+	NumCoords int
+	ListenAddr string
 }
 
 func main() {
 	config := loadConfig()
 	listenerReady := make(chan bool, 1)
-	connections := map[int]*net.TCPConn{}
+	connections := make(map[int]*net.TCPConn)
 	
 	go listenForCoordinators(config, listenerReady, connections)
 	
@@ -30,10 +31,12 @@ func main() {
 	fmt.Println("Sweet, let's go")
 	
 	launchCoordinators(config)
+	
+	_ = <- listenerReady
 }
 
 func loadConfig() Config {
-	var config = Config{"", 0, 4009}
+	var config = Config{"", 0, "127.0.0.1:4009"}
 	
 	configFile, err := os.Open("config.json", os.O_RDONLY, 0)
 	if err != nil {
@@ -53,7 +56,7 @@ func loadConfig() Config {
 func launchCoordinators(config Config) {
 	wd, err := os.Getwd()
 	for i := 0; i < 10; i++ {
-		argv := []string{strconv.Itoa(i), strconv.Uitoa(config.ListenPort)}
+		argv := []string{strconv.Itoa(i), config.ListenAddr}
 		_, err = os.ForkExec(config.CoordPath, argv, 
 							 nil, wd, []*os.File{nil, os.Stdout, os.Stderr})
 		if err != nil {
@@ -64,21 +67,45 @@ func launchCoordinators(config Config) {
 }
 
 func listenForCoordinators(config Config, ready chan bool, connections map[int]*net.TCPConn) {
-	addr, err := net.ResolveTCPAddr("127.0.0.1:4009");
+	addr, err := net.ResolveTCPAddr(config.ListenAddr);
 	if err != nil { log.Exit(err) }
 	listener, err := net.ListenTCP("tcp", addr);
 	if err != nil { log.Exit(err) }
 	
-	for {
+	fmt.Println("OK TO ISSUE")
+	ready <- true
+	
+	for ; len(connections) < config.NumCoords; {
 		conn, err := listener.AcceptTCP();
 		if err != nil {
 			log.Println("error in Accept():", err)
 		} else {
-			conn.SetKeepAlive(true);
-			conn.SetReadTimeout(30000);
+			conn.SetKeepAlive(true)
+			conn.SetReadTimeout(30000)
+			rcvd := make([]byte, 127)
+			size, err := conn.Read(rcvd)
+			if err != nil { 
+				if err == os.EAGAIN {
+					log.Println("Recoverable?")
+					for size, err = conn.Read(rcvd); err == os.EAGAIN;  {
+						
+					}
+				} else {
+					log.Println("Non-recoverable error")
+					log.Exit(err)
+				}
+			}
+			if size > 0 {
+				numString := string(rcvd[0:size])
+				id, err := strconv.Atoi(numString)
+				if err != nil { log.Exit(err) }
+				connections[id] = conn
+			}
+			time.Sleep(10000000)
 		}
 	}
 	
-	fmt.Println("OK TO ISSUE")
+	fmt.Println("Connected to all coordinators!")
+	
 	ready <- true
 }
