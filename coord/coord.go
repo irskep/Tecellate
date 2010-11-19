818c1e9b3e2989e144679b28a2bb4d90d4978a70
Stephen Johnson <srj15@case.edu>
2010-11-19 18:03:23 -0500

    Refactoring

--- coord/coord.go
+++ coord/coord.go
@@ -18,7 +18,7 @@ type BotConf struct {
 	Path string
 }
 
-func main() {
+func setupConnection() *net.TCPConn {
 	fmt.Printf("Launching with address: %s\n", os.Args[1])
 	addr, err := net.ResolveTCPAddr(os.Args[1]);
 	if err != nil { log.Exit(err) }
@@ -35,14 +35,22 @@ func main() {
 	
 	fmt.Printf("Waiting for connections on %s...\n", addr)
 	
+	return conn
+}
+
+func receive_from_master(conn *net.TCPConn) []byte {
 	rcvd := make([]byte, 4096)
 	size, err := conn.Read(rcvd)
 	if err != nil { log.Exit("Error in recv:", err) }
+	return rcvd[0:size]
+}
+
+func main() {
+	conn := setupConnection()
 	
 	config := CoordConfig{0, nil}
-	err = json.Unmarshal(rcvd, config)
-	
-	fmt.Printf("Received: %s\n", rcvd[0:size])
+	err := json.Unmarshal(receive_from_master(conn), config)
+	if err != nil { log.Exit("JSON error:", err) }
 	
 	conn.Write([]uint8("ok"))
 }
