aab0daf2497f6639528f3f61e2798bf84d84c9c0
Stephen Johnson <srj15@case.edu>
2010-11-19 17:54:04 -0500

    Improved connection reliability

--- coord/coord.go
+++ coord/coord.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"fmt"
+	"json"
 	"os"
 	"net"
 	"log"
+	"time"
 )
 
+type CoordConfig struct {
+	identifier int
+	BotConfs []BotConf
+}
+
+type BotConf struct {
+	Path string
+}
+
 func main() {
-	fmt.Printf("Coordinator %s\n", os.Args[0])
+	fmt.Printf("Launching with address: %s\n", os.Args[1])
 	addr, err := net.ResolveTCPAddr(os.Args[1]);
-	if err != nil { log.Printf("%s: ", os.Args[0]); log.Exit(err) }
-	conn, err := net.DialTCP("tcp", nil, addr)
-	if err != nil { log.Printf("%s: ", os.Args[0]); log.Exit(err) }
-	conn.Write([]uint8(os.Args[0]))
-	conn.Close()
+	if err != nil { log.Exit(err) }
+	listener, err := net.ListenTCP("tcp", addr);
+	if err != nil { log.Exit(err) }
+	
+	conn, err := listener.AcceptTCP();
+	if err != nil { log.Exit("error in Accept():", err) }
+	defer conn.Close()
+	conn.SetKeepAlive(true)
+	conn.SetReadTimeout(30000)
+	
+	time.Sleep(1000000)
+	
+	fmt.Printf("Waiting for connections on %s...\n", addr)
+	
+	rcvd := make([]byte, 4096)
+	size, err := conn.Read(rcvd)
+	if err != nil { log.Exit("Error in recv:", err) }
+	
+	config := CoordConfig{0, nil}
+	err = json.Unmarshal(rcvd, config)
+	
+	fmt.Printf("Received: %s\n", rcvd[0:size])
+	
+	conn.Write([]uint8("ok"))
 }
--- server/tecellate.go
+++ server/tecellate.go
@@ -6,106 +6,79 @@ import (
 	"json"
 	"io/ioutil"
 	"log"
-	"strconv"
 	"net"
-	"time"
+	"rand"
 )
 
 type Config struct {
-	CoordPath string
+	Coords []string
 	NumCoords int
 	ListenAddr string
+	BotDefs []BotDef
+}
+
+type BotDef struct {
+	Path string
+	Count int
+}
+
+type CoordConfig struct {
+	identifier int
+	BotConfs []BotConf
+}
+
+type BotConf struct {
+	Path string
 }
 
 func main() {
 	config := loadConfig()
-	listenerReady := make(chan bool, 1)
-	connections := make(map[int]*net.TCPConn)
-	
-	go listenForCoordinators(config, listenerReady, connections)
-	
-	fmt.Println("Launched getCoordinators, waiting for ok...")
-	
-	_ = <- listenerReady
-	
-	fmt.Println("Sweet, let's go")
-	
-	launchCoordinators(config)
-	
-	_ = <- listenerReady
+	connections := connectToCoordinators(config)
+	coordConfigs := configureCoordinators(config)
+	for i, conn := range(connections) {
+		data, err := json.Marshal(coordConfigs[i])
+		if (err != nil) { log.Exit(err) }
+		conn.Write(data)
+	}
 }
 
-func loadConfig() Config {
-	var config = Config{"", 0, "127.0.0.1:4009"}
+func loadConfig() *Config {
+	config := new(Config)
 	
 	configFile, err := os.Open("config.json", os.O_RDONLY, 0)
-	if err != nil {
-		log.Exit(err)
-	}
+	if err != nil { log.Exit(err) }
 	defer configFile.Close()
 	
 	configBytes, err := ioutil.ReadAll(configFile)
 	if err != nil {
 		log.Exit(err)
 	} else {
-		json.Unmarshal(configBytes, &config)
+		json.Unmarshal(configBytes, config)
 	}
 	return config
 }
 
-func launchCoordinators(config Config) {
-	wd, err := os.Getwd()
-	for i := 0; i < 10; i++ {
-		argv := []string{strconv.Itoa(i), config.ListenAddr}
-		_, err = os.ForkExec(config.CoordPath, argv, 
-							 nil, wd, []*os.File{nil, os.Stdout, os.Stderr})
-		if err != nil {
-			log.Printf("Error on process %d:", i)
-			log.Exit(err)
-		}
+func connectToCoordinators(config *Config) ([]*net.TCPConn) {
+	connections := make([]*net.TCPConn, len(config.Coords))
+	for i, _ := range(connections) {
+		fmt.Printf("%d", i)
+		addr, err := net.ResolveTCPAddr(config.Coords[i]);
+		if err != nil { log.Exit(err) }
+		conn, err := net.DialTCP("tcp", nil, addr)
+		if err != nil { log.Exit(err) }
+		connections[i] = conn
 	}
+	return connections
 }
 
-func listenForCoordinators(config Config, ready chan bool, connections map[int]*net.TCPConn) {
-	addr, err := net.ResolveTCPAddr(config.ListenAddr);
-	if err != nil { log.Exit(err) }
-	listener, err := net.ListenTCP("tcp", addr);
-	if err != nil { log.Exit(err) }
-	
-	fmt.Println("OK TO ISSUE")
-	ready <- true
-	
-	for ; len(connections) < config.NumCoords; {
-		conn, err := listener.AcceptTCP();
-		if err != nil {
-			log.Println("error in Accept():", err)
-		} else {
-			conn.SetKeepAlive(true)
-			conn.SetReadTimeout(30000)
-			rcvd := make([]byte, 127)
-			size, err := conn.Read(rcvd)
-			if err != nil { 
-				if err == os.EAGAIN {
-					log.Println("Recoverable?")
-					for size, err = conn.Read(rcvd); err == os.EAGAIN;  {
-						
-					}
-				} else {
-					log.Println("Non-recoverable error")
-					log.Exit(err)
-				}
-			}
-			if size > 0 {
-				numString := string(rcvd[0:size])
-				id, err := strconv.Atoi(numString)
-				if err != nil { log.Exit(err) }
-				connections[id] = conn
-			}
-			time.Sleep(10000000)
+func configureCoordinators(config *Config) ([]CoordConfig) {
+	coordConfigs := make([]CoordConfig, len(config.Coords))
+	for _, bot := range(config.BotDefs) {
+		for i := 0; i < bot.Count; i++ {
+			ix := rand.Int() % len(coordConfigs)
+			newConf := BotConf{bot.Path}
+			coordConfigs[ix].BotConfs = append(coordConfigs[ix].BotConfs, newConf)
 		}
 	}
-	
-	fmt.Println("Connected to all coordinators!")
-	
-	ready <- true
+	return coordConfigs
 }
