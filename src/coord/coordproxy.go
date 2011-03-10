5a4d92661597cb2f9ef9f5907d08ccf53925f1ed
Stephen Johnson <srj15@case.edu>
2011-03-10 16:38:23 -0500

    Used timeout pattern in coord proxy

--- src/coord/coordproxy.go
+++ src/coord/coordproxy.go
@@ -2,6 +2,10 @@ package coord
 
 import geo "coord/geometry"
 
+import (
+    "time"
+)
+
 type CoordinatorProxy struct {
     parentIdentifier int
     conn chan []byte
@@ -14,8 +18,18 @@ func NewCoordProxy(parentIdentifier int, channel chan []byte) *CoordinatorProxy 
 func (self *CoordinatorProxy) RequestStatesInBox(turn int,
                                                  bottomLeft geo.Point,
                                                  topRight geo.Point) *GameStateResponse {
+    
     self.conn <- GameStateRequestJson(self.parentIdentifier, turn, bottomLeft, topRight)
-    return GameStateResponseFromJson(<- self.conn)
+    
+    timeout := time.NewTicker(5*1e9)
+    select {
+    case msg := <-self.conn:
+        timeout.Stop()
+        return GameStateResponseFromJson(msg)
+    case <-timeout.C:
+        timeout.Stop()
+    }
+    panic("RPC request timeout")
 }
 
 func (self *CoordinatorProxy) SendComplete() {
