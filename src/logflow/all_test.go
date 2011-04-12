23ba4cf3616052bb62e744ecef68f40acb5db9f7
Stephen Johnson <srj15@case.edu>
2011-04-12 17:49:16 -0400

    Wrote a mock Writer for log testing

--- src/logflow/all_test.go
+++ src/logflow/all_test.go
@@ -1,14 +1,64 @@
 package logflow
 
-import "testing"
-import "fmt"
+import (
+    "bytes"
+    "testing"
+)
+
+/*
+    Helpers
+*/
+
+type TestWriter struct {
+    Contents []byte
+}
+
+func NewTestWriter() *TestWriter {
+    return &TestWriter{
+        Contents: make([]byte, 0),
+    }
+}
+
+func (self *TestWriter) Write(p []byte) {
+    self.Contents = bytes.Join([][]byte{self.Contents, p}, []byte{})
+}
+
+func (self *TestWriter) String() string {
+    return string(self.Contents)
+}
+
+/*
+    Tests
+*/
+
+func TestTestWriter(t *testing.T) {
+    w := NewTestWriter()
+    testString := []byte("ABC\n")
+    w.Write(testString)
+    if !bytes.Equal(testString, w.Contents) {
+        t.Fatalf("Strings don't match! (%v, %v)", testString, w.Contents)
+    }
+    testString2 := []byte("DEF\n")
+    w.Write(testString2)
+    concattedTestString := []byte("ABC\nDEF\n")
+    if !bytes.Equal(concattedTestString, w.Contents) {
+        t.Fatalf("Strings don't match! (%v, %v)", testString, w.Contents)
+    }
+}
 
 func TestSourceInstantiate(t *testing.T) {
     src := NewSource("test.info")
-    fmt.Println(src)
+    t.Log(src)
 }
 
 func TestSinkInstantiate(t *testing.T) {
     sink, err := NewSink("test/info", "test/debug")
-    fmt.Println(sink, err)
+    t.Log(sink, err)
+}
+
+func TestHookup(t *testing.T) {
+    sink, err := NewSink("test/info")
+    t.Log(sink, err)
+    src := NewSource("test/info")
+    src.Println("Hello!")
 }
--- src/logflow/logflow_testmain.go
+++ src/logflow/logflow_testmain.go
@@ -11,6 +11,8 @@ import (
 var test_logflow = []testing.InternalTest {
 	testing.InternalTest{ "logflow.TestSourceInstantiate", logflow.TestSourceInstantiate },
 	testing.InternalTest{ "logflow.TestSinkInstantiate", logflow.TestSinkInstantiate },
+	testing.InternalTest{ "logflow.TestHookup", logflow.TestHookup },
+	testing.InternalTest{ "logflow.TestTestWriter", logflow.TestTestWriter },
 }
 
 
