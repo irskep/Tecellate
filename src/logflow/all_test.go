2833efede83fd6d3805363c57f1d2841502b4b60
Stephen Johnson <srj15@case.edu>
2011-04-12 20:35:44 -0400

    Simplified logging tests
    Signed-off-by: Stephen Johnson <srj15@case.edu>

--- src/logflow/all_test.go
+++ src/logflow/all_test.go
@@ -5,44 +5,29 @@ import (
     "testing"
 )
 
+func checkLog(t *testing.T, snk Sink, target string, actual *bytes.Buffer) {
+    if !bytes.Equal([]byte(target), actual.Bytes()) {
+        t.Errorf("%v mismatch:\n%v%v)", snk, target, actual.String())
+    }
+}
+
 func TestHookup(t *testing.T) {
     w1 := new(bytes.Buffer)
     w2 := new(bytes.Buffer)
     w3 := new(bytes.Buffer)
-    snk1, _ := NewSink(w1, "test1(/.*)?")
-    snk2, _ := NewSink(w2, "test2(/.*)?")
-    snk3, _ := NewSink(w3, "test1(/.*)?", "test2(/.*)?")
+    snk1, _ := NewSink(w1, "test1/.*")
+    snk2, _ := NewSink(w2, "test2/.*")
+    snk3, _ := NewSink(w3, "test1/.*", "test2/.*")
     src1 := NewSource("test1")
     src2 := NewSource("test2")
     
-    var shouldBe string
-    
     src1.Println("ABC")
-    
-    shouldBe = "test1/info: ABC\n"
-    if !bytes.Equal([]byte(shouldBe), w1.Bytes()) {
-        t.Errorf("%v mismatch:\n%v%v)", snk1, shouldBe, w1.String())
-    }
-    shouldBe = ""
-    if !bytes.Equal([]byte(shouldBe), w2.Bytes()) {
-        t.Errorf("%v mismatch:\n%v%v)", snk2, shouldBe, w2.String())
-    }
-    shouldBe = "test1/info: ABC\n"
-    if !bytes.Equal([]byte(shouldBe), w3.Bytes()) {
-        t.Errorf("%v mismatch:\n%v%v)", snk3, shouldBe, w3.String())
-    }
+    checkLog(t, snk1, "test1/info: ABC\n", w1)
+    checkLog(t, snk2, "", w2)
+    checkLog(t, snk3, "test1/info: ABC\n", w3)
     
     src2.Println("DEF")
-    shouldBe = "test1/info: ABC\n"
-    if !bytes.Equal([]byte(shouldBe), w1.Bytes()) {
-        t.Errorf("%v mismatch:\n%v%v)", snk1, shouldBe, w1.String())
-    }
-    shouldBe = "test2/info: DEF\n"
-    if !bytes.Equal([]byte(shouldBe), w2.Bytes()) {
-        t.Errorf("%v mismatch:\n%v%v)", snk2, shouldBe, w2.String())
-    }
-    shouldBe = "test1/info: ABC\ntest2/info: DEF\n"
-    if !bytes.Equal([]byte(shouldBe), w3.Bytes()) {
-        t.Errorf("%v mismatch:\n%v%v)", snk3, shouldBe, w3.String())
-    }
+    checkLog(t, snk1, "test1/info: ABC\n", w1)
+    checkLog(t, snk2, "test2/info: DEF\n", w2)
+    checkLog(t, snk3, "test1/info: ABC\ntest2/info: DEF\n", w3)
 }
