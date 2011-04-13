696a28003b8967cff5764d712e4065ce0640d95b
Tim Henderson <tim.tadh@gmail.com>
2011-04-12 22:47:26 -0400

    initialize psuedo random number generator properly
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- src/coord/game/message.go
+++ src/coord/game/message.go
@@ -1,9 +1,12 @@
 package game
 
-import "crypto/rand"
+import pseudo_rand "rand"
+import crypto_rand "crypto/rand"
+import "sort"
 import geo "coord/geometry"
 
 const MessageLength = 256
+const perfectHear = 5.0
 
 type Message interface {
     Source() *geo.Point
@@ -18,6 +21,23 @@ type sortableMessages struct {
 
 type Messages map[uint8][]Message
 
+// initializer for random number generator -------------------------------------
+func init() {
+    // This function was a originally part of the structure/block/byteslice pkg
+    // in SourceQL
+    Int64 := func (b []byte) int64 {
+        i := uint64(0)
+        for j := 0; j < len(b) && j < 8; j++ {
+            i |= 0x00000000000000ff & uint64(b[j])
+            if j+1 < len(b) {
+                i <<= 8
+            }
+        }
+        return int64(i)
+    }
+    pseudo_rand.Seed(Int64(randbytes(8)))
+}
+
 // convience functions ---------------------------------------------------------
 func randbyte() byte {
     return randbytes(1)[0]
@@ -25,13 +45,13 @@ func randbyte() byte {
 
 func randbytes(k int) []byte {
     bytes := make([]byte, k)
-    if n, err := rand.Read(bytes); n == k && err == nil {
+    if n, err := crypto_rand.Read(bytes); n == k && err == nil {
         return bytes
     }
     panic("Can't get random byte.")
 }
 
-// Messsages Methods -----------------------------------------------------------
+// Messages Methods -----------------------------------------------------------
 func (self Messages) Add(msg Message) {
     f := msg.Frequency()
     if _, has := self[f]; !has {
@@ -40,13 +60,16 @@ func (self Messages) Add(msg Message) {
     self[f] = append(self[f], msg)
 }
 
-func (self Messages) Hear(loc *geo.Point, freq uint8) []byte {
+func (self Messages) Hear(loc *geo.Point, freq uint8) (msg []byte) {
+    msg = make([]byte, MessageLength)
     if messages, has := self[freq]; has {
         msgs := newSortableMessages(len(messages), loc)
         for _, msg := range messages {
             msgs.add(msg)
         }
-        return nil
+        msgs.sort()
+
+        return
     }
     return randbytes(MessageLength)
 }
@@ -60,6 +83,10 @@ func newSortableMessages(size int, loc *geo.Point) *sortableMessages {
 }
 func (self *sortableMessages) add(msg Message) {
     self.msgs = append(self.msgs, msg)
+}
+func (self *sortableMessages) sort() *sortableMessages {
+    sort.Sort(self)
+    return self
 }
 
 // sort interface
